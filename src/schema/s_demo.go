e59ac123b32f22c887eb24e3ff904eee182cfbd2
lyric <tiannianshou@gmail.com>
2018-10-08 23:16:56 +0800

    增加数据模型

--- src/schema/s_demo.go
+++ src/schema/s_demo.go
@@ -1,6 +1,6 @@
 package schema
 
-// Demo 测试
+// Demo 示例
 type Demo struct {
 	ID   int64  `json:"id" db:"id,primarykey,autoincrement"`
 	Code string `json:"code" db:"code,size:50" binding:"required"`
