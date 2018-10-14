01edbd7325b632c5b5270e0cba4b63424cd86ff7
lyric <tiannianshou@gmail.com>
2018-10-14 10:31:43 +0800

    增加菜单管理接口

--- src/model/mysql/m_common.go
+++ src/model/mysql/m_common.go
@@ -9,11 +9,13 @@ import (
 // Common mysql存储模块
 type Common struct {
 	Demo *Demo
+	Menu *Menu
 }
 
 // Init 初始化
 func (a *Common) Init(g *inject.Graph, db *mysql.DB) *Common {
 	a.Demo = new(Demo).Init(g, db, a)
+	a.Menu = new(Menu).Init(g, db, a)
 
 	return a
 }
--- src/util/util.go
+++ src/util/util.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"reflect"
 	"strings"
 
 	"github.com/fatih/structs"
@@ -9,6 +10,22 @@ import (
 // StructToMap 将结构体转换为字典
 func StructToMap(s interface{}) map[string]interface{} {
 	return structs.Map(s)
+}
+
+// StructsToMapSlice 将结构体切片转换为字典切片
+func StructsToMapSlice(v interface{}) []map[string]interface{} {
+	iVal := reflect.Indirect(reflect.ValueOf(v))
+	if iVal.IsNil() || iVal.IsValid() || iVal.Type().Kind() != reflect.Slice {
+		return make([]map[string]interface{}, 0)
+	}
+
+	l := iVal.Len()
+	result := make([]map[string]interface{}, l)
+	for i := 0; i < l; i++ {
+		result[i] = structs.Map(iVal.Index(i).Interface())
+	}
+
+	return result
 }
 
 // Trim 去除空格
