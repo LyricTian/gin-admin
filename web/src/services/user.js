449e35dec21b064f17caaa483f2ba75bf2075b36
lyric <tiannianshou@gmail.com>
2018-12-06 18:17:09 +0800

    update request

--- web/src/services/user.js
+++ web/src/services/user.js
@@ -1,50 +1,50 @@
 import { stringify } from 'qs';
-import request, { baseURLV1 } from '../utils/request';
+import request, { v1API } from '../utils/request';
 
 const buRouter = 'users';
 
 export async function query(params) {
-  return request(`${baseURLV1}/${buRouter}?${stringify(params)}`);
+  return request(`${v1API}/${buRouter}?${stringify(params)}`);
 }
 
 export async function get(params) {
-  return request(`${baseURLV1}/${buRouter}/${params.record_id}`);
+  return request(`${v1API}/${buRouter}/${params.record_id}`);
 }
 
 export async function create(params) {
-  return request(`${baseURLV1}/${buRouter}`, {
+  return request(`${v1API}/${buRouter}`, {
     method: 'POST',
     body: params,
   });
 }
 
 export async function update(params) {
-  return request(`${baseURLV1}/${buRouter}/${params.record_id}`, {
+  return request(`${v1API}/${buRouter}/${params.record_id}`, {
     method: 'PUT',
     body: params,
   });
 }
 
 export async function del(params) {
-  return request(`${baseURLV1}/${buRouter}/${params.record_id}`, {
+  return request(`${v1API}/${buRouter}/${params.record_id}`, {
     method: 'DELETE',
   });
 }
 
 export async function delMany(params) {
-  return request(`${baseURLV1}/${buRouter}?${stringify(params)}`, {
+  return request(`${v1API}/${buRouter}?${stringify(params)}`, {
     method: 'DELETE',
   });
 }
 
 export async function enable(params) {
-  return request(`${baseURLV1}/${buRouter}/${params.record_id}/enable`, {
+  return request(`${v1API}/${buRouter}/${params.record_id}/enable`, {
     method: 'PATCH',
   });
 }
 
 export async function disable(params) {
-  return request(`${baseURLV1}/${buRouter}/${params.record_id}/disable`, {
+  return request(`${v1API}/${buRouter}/${params.record_id}/disable`, {
     method: 'PATCH',
   });
 }
