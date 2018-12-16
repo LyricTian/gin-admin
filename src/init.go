379f84248292284918cf8b52d53a5aada7c2cfa3
lyric <tiannianshou@gmail.com>
2018-12-16 15:21:05 +0800

    Refactoring dependency injection and middleware

--- src/init.go
+++ src/init.go
@@ -1,20 +1,14 @@
 package src
 
 import (
-	"database/sql"
 	"fmt"
 	"os"
-	"time"
 
+	"github.com/LyricTian/gin-admin/src/inject"
 	"github.com/LyricTian/gin-admin/src/logger"
-	model "github.com/LyricTian/gin-admin/src/model/mysql"
-	"github.com/LyricTian/gin-admin/src/service/mysql"
 	"github.com/LyricTian/gin-admin/src/util"
 	"github.com/LyricTian/gin-admin/src/web"
-	"github.com/LyricTian/gin-admin/src/web/ctl"
 	"github.com/LyricTian/logrus-mysql-hook"
-	"github.com/casbin/casbin"
-	"github.com/facebookgo/inject"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
@@ -24,20 +18,17 @@ type CloseHandle func()
 
 // Init 初始化所有服务
 func Init(version, traceID string) (*gin.Engine, CloseHandle) {
-	// 初始化MySQL
-	db := InitMySQL()
+	// 初始化依赖注入
+	obj := inject.Init()
 
 	// 初始化日志
-	loggerHook := InitLogger(db.Db)
+	loggerHook := InitLogger(obj)
 
 	logger.System(traceID).Infof("服务已运行在[%s]模式下，版本号:%s，进程号：%d",
 		viper.GetString("run_mode"), version, os.Getpid())
 
-	// 初始化依赖注入
-	enforcer, _, ctlCommon := InitInject(db)
-
 	// 初始化HTTP服务
-	httpHandler := web.Init(db, enforcer, ctlCommon)
+	httpHandler := web.Init(obj)
 
 	return httpHandler, func() {
 		// 等待日志钩子写入完成
@@ -46,81 +37,17 @@ func Init(version, traceID string) (*gin.Engine, CloseHandle) {
 		}
 
 		// 关闭数据库
-		err := db.Close()
-		if err != nil {
-			logger.System(traceID).Errorf("关闭数据库发生错误: %s", err.Error())
+		if db := obj.MySQL; db != nil {
+			if err := db.Close(); err != nil {
+				logger.System(traceID).Errorf("关闭数据库发生错误: %s", err.Error())
+			}
 		}
+
 	}
-}
-
-// InitInject 初始化依赖注入
-func InitInject(db *mysql.DB) (*casbin.Enforcer, *model.Common, *ctl.Common) {
-	g := new(inject.Graph)
-
-	// 注入casbin
-	enforcer := casbin.NewEnforcer(viper.GetString("casbin_model"), false)
-	g.Provide(&inject.Object{Value: enforcer})
-
-	// 注入mysql存储
-	modelCommom := new(model.Common).Init(g, db)
-
-	// 注入控制器
-	ctlCommon := new(ctl.Common)
-	g.Provide(&inject.Object{Value: ctlCommon})
-
-	if err := g.Populate(); err != nil {
-		panic("注入模块发生错误:" + err.Error())
-	}
-
-	return enforcer, modelCommom, ctlCommon
-}
-
-// InitMySQL 初始化mysql数据库
-func InitMySQL() *mysql.DB {
-	mysqlConfig := viper.GetStringMap("mysql")
-	var opts []mysql.Option
-	if v := util.T(mysqlConfig["trace"]).Bool(); v {
-		opts = append(opts, mysql.SetTrace(v))
-	}
-
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s",
-		mysqlConfig["username"],
-		mysqlConfig["password"],
-		mysqlConfig["addr"],
-		mysqlConfig["database"],
-	)
-	opts = append(opts, mysql.SetDSN(dsn))
-
-	if v := util.T(mysqlConfig["engine"]).String(); v != "" {
-		opts = append(opts, mysql.SetEngine(v))
-	}
-
-	if v := util.T(mysqlConfig["encoding"]).String(); v != "" {
-		opts = append(opts, mysql.SetEncoding(v))
-	}
-
-	if v := util.T(mysqlConfig["max_lifetime"]).Int(); v > 0 {
-		opts = append(opts, mysql.SetMaxLifetime(time.Duration(v)*time.Second))
-	}
-
-	if v := util.T(mysqlConfig["max_open_conns"]).Int(); v > 0 {
-		opts = append(opts, mysql.SetMaxOpenConns(v))
-	}
-
-	if v := util.T(mysqlConfig["max_idle_conns"]).Int(); v > 0 {
-		opts = append(opts, mysql.SetMaxIdleConns(v))
-	}
-
-	db, err := mysql.NewDB(opts...)
-	if err != nil {
-		panic("初始化MySQL数据库发生错误：" + err.Error())
-	}
-
-	return db
 }
 
 // InitLogger 初始化日志
-func InitLogger(mysqlDB *sql.DB) logger.HookFlusher {
+func InitLogger(obj *inject.Object) logger.HookFlusher {
 	logConfig := viper.GetStringMap("log")
 
 	var opts []logger.Option
@@ -153,7 +80,7 @@ func InitLogger(mysqlDB *sql.DB) logger.HookFlusher {
 			}
 
 			hook := mysqlhook.DefaultWithExtra(
-				mysqlDB,
+				obj.MySQL.Db,
 				fmt.Sprintf("%s_%s",
 					viper.GetStringMap("mysql")["table_prefix"],
 					util.T(hookConfig["table"]).String()),
