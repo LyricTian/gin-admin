37029f07d2160f4a3e46f950b680fde16088ed11
lyric <tiannianshou@gmail.com>
2019-01-12 17:19:25 +0800

    Fixed test

--- src/web/context/util_test.go
+++ src/web/context/util_test.go
@@ -5,8 +5,12 @@ import "testing"
 func TestRouterTitle(t *testing.T) {
 	SetRouterTitle("GET", "/api/v1/test/:id/status", "test id")
 
-	title := GetRouterTitle("GET", "/api/v1/test/123/status")
+	title, key := GetRouterTitleAndKey("GET", "/api/v1/test/123/status")
 	if title != "test id" {
 		t.Error("not the expected value:", title)
+	}
+
+	if key != "GET-/api/v1/test/:id/status" {
+		t.Error("not the expected value:", key)
 	}
 }
