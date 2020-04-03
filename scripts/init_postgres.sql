7f05faccb36a18fef32ac3171be481adf137c441
tiannianshou <tiannianshou@gmail.com>
2020-04-03 17:15:12 +0800

    Fixed config

--- scripts/init_postgres.sql
+++ scripts/init_postgres.sql
@@ -1,2 +1,2 @@
 -- Create a database
-create database ginadmin with encoding = 'UTF8' LC_CTYPE = 'en_US.UTF-8' LC_COLLATE = 'en_US.UTF-8' template = template1;
+create database gin-admin with encoding = 'UTF8' LC_CTYPE = 'en_US.UTF-8' LC_COLLATE = 'en_US.UTF-8' template = template1;
